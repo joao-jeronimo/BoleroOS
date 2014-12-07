a480d88b2547d74849c0bb2617dd447619455241
JJBO <joao.jeronimo@ist.utl.pt>
2014-12-07 04:13:31 +0000

    Stuff.....

--- experiences/buildpen.sh
+++ experiences/buildpen.sh
@@ -12,3 +12,11 @@ export MTOOLSRC="../mtools.conf"
 # Format pendrive partition...
 mformat p:
 
+# Now we are supposed to run some commands to install grub in the pen drive...
+grub-mkimage -c myconf.grub -o mygrub.img -O i386-pc normal fat configfile
+dd if=/boot/grub/i386-pc/boot.img of=pendrv.img bs=1 count=446 conv=notrunc
+#grub-install.real pendrv.img
+dd if=mygrub.img of=pendrv.img bs=1 seek=512 conv=notrunc
+
+# Run the beast!
+qemu-system-i386 -usbdevice disk:format=raw:pendrv.img
